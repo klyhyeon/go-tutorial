784f83b1ee225c22d3421106e8819c6f835b57d1
yuhyeon <klyhyeon@gmail.com>
2024-10-07 08:58:18 +0900

    feat: handle errors

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,14 +1,31 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	// Return a greeting that embeds the name in a message.
 
 	/* var message string // long way to initialize var
 	/message = fmt.Sprintf("Hi, %v. Welcome!", name)
 	*/
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
+}
+
+func Again(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	message := fmt.Sprintf("Hi, %v. Welcome again!", name)
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"go-tutorial/greetings"
 )
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	message, err := greetings.Hello("")
+	// If an error was returned, print it to the console and exit.
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// Get a greeting message and print it.
-	message := greetings.Hello("Yuhyeon")
+	//message := greetings.Hello("Yuhyeon")
 	fmt.Println(message)
 }
